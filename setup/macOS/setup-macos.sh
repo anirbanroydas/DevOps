ffff1ba869d0889e0d38484a8305e20d3f45cbcd
Anirban Roy Das <anirban.nick@gmail.com>
2017-02-07 16:46:36 +0530

    added notes to setup enviroment for mac os like docker toolbox installation

---
+++ setup/macOS/setup-macos.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# first downlaod the docker-toolbox
+# docker-toolbox will download the follwoing for you automatically:
+# docker engine
+# docker engine cli
+# docker-machine 
+# docker-compose
+# virtualbox
+# kitematic
+
+# Docker Toolbox includes the following Docker tools:
+
+# Docker CLI client for running Docker Engine to create images and containers
+# Docker Machine so you can run Docker Engine commands from macOS terminals
+# Docker Compose for running the docker-compose command
+# Kitematic, the Docker GUI
+# the Docker QuickStart shell preconfigured for a Docker command-line environment
+# Oracle VM VirtualBox
+#
+
+
+## NOTE ## : for more detail check out this link https://docs.docker.com/toolbox/toolbox_install_mac
+
+## NOTE ## : If you have macOS 10.10.3 Yosemite or newer, consider using Docker for Mac (not docket toolbox)
+## NOTE ## : sometimes even an older system pre mid 2010 system donot support docker for mac ( in that case use docker toolbox)
+
+
+## NOTE ## : For docker for mac follow this link https://docs.docker.com/docker-for-mac
