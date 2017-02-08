9ff08da1e0b6a3eead29b31db142dac5b7f69c03
Anirban Roy Das <anirban.nick@gmail.com>
2017-02-09 04:22:14 +0530

    added env for provisioing/production

---
+++ provisioning/production/aws/docker-machine/provision-cluster-remove.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+export ENV_INIT_SCRIPT_PATH=$HOME/Documents/Github/sources/public/DevOps/provisioning/production
+
+source $ENV_INIT_SCRIPT_PATH/env_init.sh
+
+# stop machines irrespective of currently running or not, irrespective of node exists or not
+# since the error messages are redirected to /dev/null
+manager_index=0
+worker_index=0
+
+for i in $(seq 0 $((CLUSTER_SIZE-1)));
+do
+
+	if [ $i -lt $MANAGER_COUNT ];
+	then
+		CLUSTER_NODE_NAME=${CLUSTER_MANAGER_NAMES[$manager_index]}-0$((i+1))
+		manager_index=$((manager_index + 1))
+	else
+		CLUSTER_NODE_NAME=${CLUSTER_WORKER_NAMES[$worker_index]}-0$((i+1))
+		worker_index=$((worker_index + 1))
+	fi
+
+	echo "stopping and removing node : $CLUSTER_NODE_NAME..."
+	docker-machine stop "$CLUSTER_NODE_NAME" > /dev/null 2>&1
+	docker-machine rm --force -y "$CLUSTER_NODE_NAME" > /dev/null 2>&1
+	echo "node stopped and removed succesfully"
+
+done
+
+
+# list the cluster machines
+echo "Current Docker Hosts:"
+docker-machine ls
+
+
+
