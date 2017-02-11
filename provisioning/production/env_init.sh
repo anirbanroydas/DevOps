7023661d2a18454a88f4fba610cdb31d5ee8a3f7
Anirban Roy Das <anirban.nick@gmail.com>
2017-02-11 14:13:41 +0530

    added provisioning/production/env_init.sh

--- provisioning/production/env_init.sh
+++ provisioning/production/env_init.sh
@@ -5,6 +5,7 @@ echo "Environment : $ENVIRONMENT"
 echo "Cloud Provider : $CLOUD_PROVIDER"
 echo "Provisioning Tool : $PROVISIONING_TOOL"
 
+source $ENV_PATH/$ENVIRONMENT/.env
 source $ENV_PATH/$ENVIRONMENT/$CLOUD_PROVIDER/$PROVISIONING_TOOL/.env
 
 [ -z "$CLUSTER_SIZE" ] &&  CLUSTER_SIZE=5
@@ -15,3 +16,9 @@ echo "Cluser Size: ${CLUSTER_SIZE}"
 
 
 source $ENV_PATH/$ENVIRONMENT/cluster-node-names
+source $ENV_PATH/$ENVIRONMENT/open_ports
+
+echo "Random Open Ports for debug pupose"
+echo "Open Ports TCP Internet [2]: ${OPEN_PORTS_TCP_INTERNET[2]}"
+echo "Open Ports TCP SWARM [1]: ${OPEN_PORTS_TCP_SWARM[1]}"
+echo "Open Ports UDP SWARM [0]: ${OPEN_PORTS_UDP_SWARM[0]}"
