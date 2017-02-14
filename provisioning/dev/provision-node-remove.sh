743972bbdba881916fbdf1ed3070ecad933bf8dd
Anirban Roy Das <anirban.nick@gmail.com>
2017-02-15 02:49:02 +0530

    Add command line argument parsing for node name

--- provisioning/dev/provision-node-remove.sh
+++ provisioning/dev/provision-node-remove.sh
@@ -5,6 +5,10 @@ source .env
 # stop machines irrespective of currently running or not, irrespective of node exists or not
 # since the error messages are redirected to /dev/null
 
+if [ "$#" -eq 1 ]; then
+	export NODE_NAME="$1"
+fi
+
 echo "[$NODE_NAME] - stopping and removing node..."
 # docker-machine stop "$NODE_NAME" > /dev/null 2>&1
 docker-machine rm --force -y "$NODE_NAME" > /dev/null 2>&1
