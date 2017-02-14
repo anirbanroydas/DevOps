908d2e4c268d6147321c7bc58ab0a783fb8a030f
Anirban Roy Das <anirban.nick@gmail.com>
2017-02-15 02:49:10 +0530

    Add command line argument parsing for node name

--- provisioning/dev/provision-node-down.sh
+++ provisioning/dev/provision-node-down.sh
@@ -5,6 +5,10 @@ source .env
 # stop machines irrespective of currently running or not, irrespective of node exists or not
 # since the error messages are redirected to /dev/null
 
+if [ "$#" -eq 1 ]; then
+	export NODE_NAME="$1"
+fi
+
 echo "[$NODE_NAME] - stopping node..."
 docker-machine stop "$NODE_NAME" > /dev/null 2>&1
 echo "[$NODE_NAME] - node stopped succesfully"
