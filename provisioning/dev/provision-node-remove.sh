bd98eb7d008267646f158b0734a083d3c220e832
Anirban Roy Das <anirban.nick@gmail.com>
2017-02-14 18:56:54 +0530

    Add provisioning/dev/provision-node-remove.sh

---
+++ provisioning/dev/provision-node-remove.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source .env
+
+# stop machines irrespective of currently running or not, irrespective of node exists or not
+# since the error messages are redirected to /dev/null
+
+echo "[$NODE_NAME] - stopping and removing node..."
+# docker-machine stop "$NODE_NAME" > /dev/null 2>&1
+docker-machine rm --force -y "$NODE_NAME" > /dev/null 2>&1
+echo "[$NODE_NAME] - node stopped and removed succesfully"
+
+# list the cluster machines
+echo "Current Docker Hosts:"
+docker-machine ls
