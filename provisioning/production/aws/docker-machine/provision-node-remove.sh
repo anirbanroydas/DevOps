e0377a1d97b55f3737e97282f6ffb74f60a651d8
Anirban Roy Das <anirban.nick@gmail.com>
2017-02-15 02:51:28 +0530

    Add command line argument parsing for node name

--- provisioning/production/aws/docker-machine/provision-node-remove.sh
+++ provisioning/production/aws/docker-machine/provision-node-remove.sh
@@ -5,6 +5,11 @@ source .env
 # stop machines irrespective of currently running or not, irrespective of node exists or not
 # since the error messages are redirected to /dev/null
 
+if [ "$#" -eq 1 ]; then
+	export AWS_NODE_NAME="$1"
+fi
+
+
 echo "[$AWS_NODE_NAME] - stopping and removing node..."
 # docker-machine stop "$AWS_NODE_NAME" > /dev/null 2>&1
 docker-machine rm --force -y "$AWS_NODE_NAME" > /dev/null 2>&1
