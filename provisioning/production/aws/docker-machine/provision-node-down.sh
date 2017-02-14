484de2515e6f1497342c26530a0f3ca7ad0df711
Anirban Roy Das <anirban.nick@gmail.com>
2017-02-15 02:51:19 +0530

    Add command line argument parsing for node name

--- provisioning/production/aws/docker-machine/provision-node-down.sh
+++ provisioning/production/aws/docker-machine/provision-node-down.sh
@@ -5,6 +5,11 @@ source .env
 # stop machines irrespective of currently running or not, irrespective of node exists or not
 # since the error messages are redirected to /dev/null
 
+if [ "$#" -eq 1 ]; then
+	export AWS_NODE_NAME="$1"
+fi
+
+
 echo "[$AWS_NODE_NAME] - stopping node..."
 docker-machine stop "$AWS_NODE_NAME" > /dev/null 2>&1
 echo "[$AWS_NODE_NAME] - node stopped succesfully"
