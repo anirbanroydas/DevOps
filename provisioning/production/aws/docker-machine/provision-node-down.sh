f0798fe8f3f800075b49d7f5313f39f18992d675
Anirban Roy Das <anirban.nick@gmail.com>
2017-02-22 00:51:06 +0530

    Remove error redirection to /dev/null

--- provisioning/production/aws/docker-machine/provision-node-down.sh
+++ provisioning/production/aws/docker-machine/provision-node-down.sh
@@ -11,7 +11,7 @@ fi
 
 
 echo "[$AWS_NODE_NAME] - stopping node..."
-docker-machine stop "$AWS_NODE_NAME" > /dev/null 2>&1
+docker-machine stop "$AWS_NODE_NAME" > /dev/null
 echo "[$AWS_NODE_NAME] - node stopped succesfully"
 
 
