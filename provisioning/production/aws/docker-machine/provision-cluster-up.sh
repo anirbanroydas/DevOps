ddfae4eb18a7362764a30a28422454a8c276ae06
Anirban Roy Das <anirban.nick@gmail.com>
2017-02-22 00:47:37 +0530

    Remove error redirections, Add sudo to docker cmds, Add config cond
    Remove error redirectionxs to /dev/null to show the error output
    
    Add sudo to docker commands since the user ubuntu doesn't have sudo
    rights to docker commands until configuration of node is finished
    
    Add labels to node while creating the node with docker-machine
    
    Add labels to swam nodes while joining/initialzing swarm
    
    Add configuration condition to begin configuration of nodes only after
    checking the Configuration environment variable

--- provisioning/production/aws/docker-machine/provision-cluster-up.sh
+++ provisioning/production/aws/docker-machine/provision-cluster-up.sh
@@ -45,7 +45,10 @@ fi
 
 export MAIN_SWARM_MANAGER=${CLUSTER_MANAGER_NAMES[0]}-01
 echo "MAIN_SWARM_MANAGER : $MAIN_SWARM_MANAGER"
-export MAIN_SWARM_MANAGER_NEW="no"
+export MAIN_SWARM_MANAGER_AWS_AZ=${AWS_ZONE_MANAGER[0]}
+echo "MAIN_SWARM_MANAGER_AWS_AZ : $MAIN_SWARM_MANAGER_AWS_AZ"
+
+export MAIN_SWARM_MANAGER_NEW=no
 
 export REMOVE="docker-machine rm --force -y "
 export START="docker-machine start "
@@ -55,41 +58,27 @@ export STOP="docker-machine stop "
 
 function create_node() {
    
-	# check if creating main master node too,
-	# if yes, then mark it as newly creted/started
-	# so that we will leave the earlier swarm forcefullly and 
-	# update all nodes swarm configuration,
-	# if not, then we will not do a forcefull swarm leave and rejoin
-	if [ "$MAIN_SWARM_MANAGER" == "$5" ]; then
-		MAIN_SWARM_MANAGER_NEW="yes"
-	fi
-
 	local CREATE="docker-machine create \
    		--amazonec2-zone $1 \
    		--amazonec2-tags $2 \
    		--amazonec2-instance-type $3 \
    		--amazonec2-root-size $4 \
+   		--engine-label io.aroyd.machine.aws.region=$6 \
+   		--engine-label io.aroyd.machine.aws.az=$7 \
+   		--engine-label io.aroyd.machine.name=$8 \
    		$5"
    
    	# echo "CREATE : $CREATE"
    	# echo "creating node..."
-   	$CREATE > /dev/null 2>&1
+   	$CREATE
 
 }
 
 
 
 function start_node() {
-	# check if starting main master node too,
-	# if yes, then mark it as newly started/created
-	# so that we will leave the earlier swarm forcefullly and 
-	# update all nodes swarm configuration,
-	# if not, then we will not do a forcefull swarm leave and rejoin
-	if [ "$MAIN_SWARM_MANAGER" == "$1" ]; then
-		MAIN_SWARM_MANAGER_NEW="yes"
-	fi
 
-	$START $1  > /dev/null 2>&1
+	$START $1  > /dev/null
 
 }
 
@@ -97,7 +86,7 @@ function start_node() {
 
 
 function regenerate_certificates_docker() {
-	docker-machine regenerate-certs --force $1 > /dev/null 2>&1 
+	docker-machine regenerate-certs --force $1 > /dev/null 
 }
 
 
@@ -108,7 +97,7 @@ function aws_ec2_authorize_security_group_ingress_swarm() {
   --group-name "$AWS_SECURITY_GROUP" \
   --source-group "$AWS_SECURITY_GROUP" \
   --protocol $1 \
-  --port $2  > /dev/null 2>&1 
+  --port $2  > /dev/null 2>&1
 
 }
 
@@ -127,11 +116,11 @@ function aws_ec2_authorize_security_group_ingress_internet() {
 
 function change_docker_env_to() {
 
-	eval $(docker-machine env $1) > /dev/null 2>&1
+	eval $(docker-machine env $1) > /dev/null
 	if [ $? -ne 0 ]; then
 		echo "Error in changing docker environment, regenerating certs..."
 		docker-machine regenerate-certs --force $1
-		eval $(docker-machine env $1) > /dev/null 2>&1
+		eval $(docker-machine env $1) > /dev/null
 	fi
 }
 
@@ -139,7 +128,7 @@ function change_docker_env_to() {
 
 function inspect_docker_node() {
 
-	docker-machine ssh $1 docker node inspect $2 > /dev/null 2>&1
+	docker-machine ssh $1 sudo docker node inspect $2 > /dev/null 2>&1
 }
 
 
@@ -178,30 +167,46 @@ do
 	if [ $? -ne 0 ];
 	then
 		# create the swarm node
+		# check if creating main master node too,
+		# if yes, then mark it as newly creted/started
+		# so that we will leave the earlier swarm forcefullly and 
+		# update all nodes swarm configuration,
+		# if not, then we will not do a forcefull swarm leave and rejoin
+		if [ "$MAIN_SWARM_MANAGER" == "$CLUSTER_NODE_NAME" ]; then
+			MAIN_SWARM_MANAGER_NEW=yes
+		fi
 		echo "[$CLUSTER_NODE_NAME] - creating Swarm ${NODE_TYPE} Node..."
 		(
-			create_node  "$AWS_ZONE"  "$AWS_TAGS"  "$AWS_INSTANCE_TYPE"  "$AWS_ROOT_SIZE"  "$CLUSTER_NODE_NAME"
+			create_node  "$AWS_ZONE"  "$AWS_TAGS"  "$AWS_INSTANCE_TYPE"  "$AWS_ROOT_SIZE"  "$CLUSTER_NODE_NAME" "$AWS_DEFAULT_REGION" "$AWS_ZONE" "$CLUSTER_NODE_NAME"
 			echo "[$CLUSTER_NODE_NAME] - Swarm ${NODE_TYPE} Node created"
 		) &
 
 	else
 		echo "[$CLUSTER_NODE_NAME] - Swarm ${NODE_TYPE} Node already exists"
 		echo "[$CLUSTER_NODE_NAME] - checking ${NODE_TYPE} Node status, start if currently stopped, otherwise move forward"
-		(
-			docker-machine status "$CLUSTER_NODE_NAME" | grep -w "Stopped" > /dev/null 2>&1
-			if [ $? -eq 0 ];
-			then
-				# start the stopped cluster node machine
-				echo "[$CLUSTER_NODE_NAME] machine is Stopped, hence starting..."
+		
+		docker-machine status "$CLUSTER_NODE_NAME" | grep -w "Stopped" > /dev/null 2>&1
+		if [ $? -eq 0 ];
+		then
+			# start the stopped cluster node machine
+			# check if creating main master node too,
+			# if yes, then mark it as newly creted/started
+			# so that we will leave the earlier swarm forcefullly and 
+			# update all nodes swarm configuration,
+			# if not, then we will not do a forcefull swarm leave and rejoin
+			if [ "$MAIN_SWARM_MANAGER" == "$CLUSTER_NODE_NAME" ]; then
+				MAIN_SWARM_MANAGER_NEW=yes
+			fi
+			echo "[$CLUSTER_NODE_NAME] machine is Stopped, hence starting..."
+			(
 				start_node "$CLUSTER_NODE_NAME"
 				echo "[$CLUSTER_NODE_NAME] machine started Successfully"
-			else
-				echo "[$CLUSTER_NODE_NAME] machine is already running, moving forward"
-			fi
-		) &
+			) &
+		else
+			echo "[$CLUSTER_NODE_NAME] machine is already running, moving forward"
+		fi
+	
 	fi
-
-
 
 done
 
@@ -304,40 +309,57 @@ if [ "$CREATE_SWARM" == "yes" ]; then
 		echo "Main Swarm Manger Node has been created/started, hence new ip, thus reinitialzing swarm"
 		echo "First leaving previous swarm, if any"
 		docker-machine ssh "$MAIN_SWARM_MANAGER" <<- EOSSH
-			docker swarm leave --force > /dev/null 2>&1
+			sudo docker swarm leave --force > /dev/null 2>&1
 			echo "Initializing new swarm..."
-			docker swarm init --advertise-addr "$MAIN_SWARM_MANAGER_PRIVATE_IP" > /dev/null 2>&1 
+			sudo docker swarm init --advertise-addr "$MAIN_SWARM_MANAGER_PRIVATE_IP" > /dev/null 
 			echo "Swarm Initialized"
+			echo "Adding Labels to Node"
+			sudo docker node update --label-add io.aroyd.swarm.cluster.node.aws.region="$AWS_DEFAULT_REGION" \
+			--label-add io.aroyd.swarm.cluster.node.aws.az="$MAIN_SWARM_MANAGER_AWS_AZ" \
+			--label-add io.aroyd.swarm.cluster.node.name="$MAIN_SWARM_MANAGER" \
+			"$MAIN_SWARM_MANAGER"
+			
+			if [ $? -ne 0 ]; then
+				echo "[$MAIN_SWARM_MANAGER] - Labels Unable to Add due to above error"
+			else
+				echo "[$MAIN_SWARM_MANAGER] - Labels Added Successfully"
+			fi
+
 		EOSSH
 	else 
 		# initialize swarm only if it is already not initialzed
 		echo "Main Swarm Manager has not been creted or restarded, hence now checking if swarm is already initialzed or not.."
 		docker-machine ssh "$MAIN_SWARM_MANAGER" <<- EOSSH
-			docker node ls | grep "Leader" > /dev/null 2>&1
+			sudo docker node ls > /dev/null 2>&1 | grep "Leader" > /dev/null 2>&1
 			if [ $? -ne 0 ]; 
 			then
 				# initialize swarm mode
 				echo "Swarm not initialzed, hence starting..."
 				echo "Initializing Swarm..."
-				docker swarm init --advertise-addr "$MAIN_SWARM_MANAGER_PRIVATE_IP" > /dev/null 2>&1
+				sudo docker swarm init --advertise-addr "$MAIN_SWARM_MANAGER_PRIVATE_IP" > /dev/null
 				echo "Swarm Initialized"
+				echo "Adding Labels to Node"
+				sudo docker node update --label-add io.aroyd.swarm.cluster.node.aws.region="$AWS_DEFAULT_REGION" \
+				--label-add io.aroyd.swarm.cluster.node.aws.az="$MAIN_SWARM_MANAGER_AWS_AZ" \
+				--label-add io.aroyd.swarm.cluster.node.name="$MAIN_SWARM_MANAGER" \
+				"$MAIN_SWARM_MANAGER"
+
+				if [ $? -ne 0 ]; then
+					echo "[$MAIN_SWARM_MANAGER] - Labels Unable to Add due to above error"
+				else
+					echo "[$MAIN_SWARM_MANAGER] - Labels Added Successfully"
+				fi
 			else
 				echo "Swarm already initailized, moving forward"
 			fi
+
 		EOSSH
 	fi
-	# echo "First leaving previous swarm, if any"
-	# docker-machine ssh "$MAIN_SWARM_MANAGER" <<- EOSSH
-	# 	docker swarm leave --force > /dev/null 2>&1
-	# 	echo "Initializing new swarm..."
-	# 	docker swarm init --advertise-addr "$MAIN_SWARM_MANAGER_PRIVATE_IP" > /dev/null 2>&1 
-	# 	echo "Swarm Initialized"
-	# EOSSH
 
 
 	# save the swarm token to use in the rest of the nodes
-	export SWARM_WORKER_JOIN_TOKEN=$(docker-machine ssh "$MAIN_SWARM_MANAGER" docker swarm join-token -q worker)
-	export SWARM_MANAGER_JOIN_TOKEN=$(docker-machine ssh "$MAIN_SWARM_MANAGER" docker swarm join-token -q manager)
+	export SWARM_WORKER_JOIN_TOKEN=$(docker-machine ssh "$MAIN_SWARM_MANAGER" sudo docker swarm join-token -q worker)
+	export SWARM_MANAGER_JOIN_TOKEN=$(docker-machine ssh "$MAIN_SWARM_MANAGER" sudo docker swarm join-token -q manager)
 
 	# initialize the managers to join the swarm
 	# but, before that check if the node has already joined the swarm as manager or not
@@ -351,10 +373,12 @@ if [ "$CREATE_SWARM" == "yes" ]; then
 		if [ $i -lt $MANAGER_COUNT ];
 		then
 			CLUSTER_NODE_NAME=${CLUSTER_MANAGER_NAMES[$manager_index]}-0$((i+1))
+			AWS_ZONE=${AWS_ZONE_MANAGER[$manager_index]}
 			SWARM_JOIN_TOKEN=$SWARM_MANAGER_JOIN_TOKEN
 			manager_index=$((manager_index + 1))
 		else
 			CLUSTER_NODE_NAME=${CLUSTER_WORKER_NAMES[$worker_index]}-0$((i+1))
+			AWS_ZONE=${AWS_ZONE_WORKER[$worker_index]}
 			SWARM_JOIN_TOKEN=$SWARM_WORKER_JOIN_TOKEN
 			worker_index=$((worker_index + 1))
 		fi
@@ -362,22 +386,42 @@ if [ "$CREATE_SWARM" == "yes" ]; then
 		echo "[$CLUSTER_NODE_NAME] - Inspecting..."
 		(
 			inspect_docker_node "$MAIN_SWARM_MANAGER"  "$CLUSTER_NODE_NAME"
-			# docker-machine ssh "$MAIN_SWARM_MANAGER" docker node inspect "$CLUSTER_NODE_NAME" > /dev/null 2>&1
 	    	if [ $? -ne 0 ]; 
 			then
-				echo "$CLUSTER_NODE_NAME node have not joined $MAIN_SWARM_MANAGER manager"
+				echo "[$CLUSTER_NODE_NAME] - node have not joined $MAIN_SWARM_MANAGER manager"
 				# change_docker_env_to "$CLUSTER_NODE_NAME"
 				# echo "Active Machine : $(docker-machine active)"
-				echo "$CLUSTER_NODE_NAME node joining swarm mananger $MAIN_SWARM_MANAGER..."
+				echo "[$CLUSTER_NODE_NAME] - node joining swarm mananger $MAIN_SWARM_MANAGER..."
 				# first leave any previous swarm if at all
 				docker-machine ssh "$CLUSTER_NODE_NAME" <<- EOSSH
-					docker swarm leave --force > /dev/null 2>&1
-					docker swarm join --token  "$SWARM_JOIN_TOKEN"  "$MAIN_SWARM_MANAGER_PRIVATE_IP":2377 > /dev/null 2>&1
-					echo "$CLUSTER_NODE_NAME joined swarm managed by $MAIN_SWARM_MANAGER"
+					sudo docker swarm leave --force > /dev/null 2>&1
+					sudo docker swarm join --token  "$SWARM_JOIN_TOKEN"  "$MAIN_SWARM_MANAGER_PRIVATE_IP":2377 > /dev/null
+					if [ $? -ne 0 ]; then
+						echo "[$CLUSTER_NODE_NAME] - Unable to join swarm managed by $MAIN_SWARM_MANAGER"
+					else
+						echo "[$CLUSTER_NODE_NAME] - joined swarm managed by $MAIN_SWARM_MANAGER"
+					fi
+					
 				EOSSH
+
+				echo "[$CLUSTER_NODE_NAME] - Adding Labels to Node" 
+				docker-machine ssh "$MAIN_SWARM_MANAGER" <<- EOSSH
+					sudo docker node update --label-add io.aroyd.swarm.cluster.node.aws.region="$AWS_DEFAULT_REGION" \
+					--label-add io.aroyd.swarm.cluster.node.aws.az="$AWS_ZONE" \
+					--label-add io.aroyd.swarm.cluster.node.name="$CLUSTER_NODE_NAME" \
+					"$CLUSTER_NODE_NAME"
+
+					if [ $? -ne 0 ]; then
+						echo "[$CLUSTER_NODE_NAME] - Labels Unable to Add due to above error"
+					else
+						echo "[$CLUSTER_NODE_NAME] - Labels Added Successfully"
+					fi
+				EOSSH
+		   		
 			else
-				echo "$CLUSTER_NODE_NAME node already joined $MAIN_SWARM_MANAGER manager, moving forward"
+				echo "[$CLUSTER_NODE_NAME] - node already joined $MAIN_SWARM_MANAGER manager, moving forward"
 			fi
+		
 		) &
 
 		# change_docker_env_to "$MAIN_SWARM_MANAGER"
@@ -390,7 +434,7 @@ if [ "$CREATE_SWARM" == "yes" ]; then
 	echo "Swarm Nodes Initialization completed"
 
 	echo "Current Swarm Nodes:"
-	docker-machine ssh "$MAIN_SWARM_MANAGER" docker node ls
+	docker-machine ssh "$MAIN_SWARM_MANAGER" sudo docker node ls
 
 else 
 
@@ -402,67 +446,89 @@ fi
 manager_index=0
 worker_index=0
 
-echo "Configuring Each Node..."
-# install docker-compose in each node
-for i in $(seq 0 $((CLUSTER_SIZE-1)));
-do
 
-	if [ $i -lt $MANAGER_COUNT ];
-	then
-		CLUSTER_NODE_NAME=${CLUSTER_MANAGER_NAMES[$manager_index]}-0$((i+1))
-		manager_index=$((manager_index + 1))
-	else
-		CLUSTER_NODE_NAME=${CLUSTER_WORKER_NAMES[$worker_index]}-0$((i+1))
-		worker_index=$((worker_index + 1))
-	fi
+if [ "$CONFIGURATION" == "yes" ]; then
 
-	echo "[$CLUSTER_NODE_NAME] - processing for swarm node..."
-	(
-		docker-machine ssh "$CLUSTER_NODE_NAME"  <<- EOSSH
-			if ! which docker-compose > /dev/null 2>&1; then 
-				echo "[$CLUSTER_NODE_NAME] - Installing docker-compose..."
-				sudo curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" \
-				-o /usr/local/bin/docker-compose > /dev/null 2>&1; 
-				sudo chmod +x /usr/local/bin/docker-compose; 
+	echo "Configuring Each Node..."
+	# install docker-compose in each node
+	for i in $(seq 0 $((CLUSTER_SIZE-1)));
+	do
+		if [ $i -lt $MANAGER_COUNT ];
+		then
+			CLUSTER_NODE_NAME=${CLUSTER_MANAGER_NAMES[$manager_index]}-0$((i+1))
+			manager_index=$((manager_index + 1))
+		else
+			CLUSTER_NODE_NAME=${CLUSTER_WORKER_NAMES[$worker_index]}-0$((i+1))
+			worker_index=$((worker_index + 1))
+		fi
+
+		echo "[$CLUSTER_NODE_NAME] - processing for swarm node..."
+		(
+			docker-machine ssh "$CLUSTER_NODE_NAME"  <<- EOSSH
+
+				if ! which docker-compose > /dev/null 2>&1; then 
+					echo "[$CLUSTER_NODE_NAME] - Installing docker-compose..."
+					echo "[$CLUSTER_NODE_NAME] : Docker Compose Version : $DOCKER_COMPOSE_VERSION"
+					sudo curl -L https://github.com/docker/compose/releases/download/"$DOCKER_COMPOSE_VERSION"/docker-compose-\$(uname -s)-\$(uname -m) -o /usr/local/bin/docker-compose > /dev/null
+					if [ $? -ne 0 ]; then
+						echo "[$CLUSTER_NODE_NAME] - Unable to install docker-compose due to above error"
+					else
+						sudo chmod +x /usr/local/bin/docker-compose 
+					fi
+					
+				else
+					echo "[$CLUSTER_NODE_NAME] - docker-compose already installed, moving forward"
+				fi
+
+				echo "[$CLUSTER_NODE_NAME] - adding user ubuntu to group docker"
+				sudo usermod -aG docker ubuntu > /dev/null
+				if [ $? -ne 0 ]; then 
+					echo "[$CLUSTER_NODE_NAME] - user ubuntu unable to be added to group docker"
+				else
+					echo "[$CLUSTER_NODE_NAME] - user ubuntu added to group docker successfully"
+				fi
+
+				if ! which rexray > /dev/null 2>&1; then 
+					echo "[$CLUSTER_NODE_NAME] - Installing Rex-Ray"
+					curl -sSL https://dl.bintray.com/emccode/rexray/install | sh
+					if [ $? -ne 0 ]; then
+						echo "[$CLUSTER_NODE_NAME] - Rex-Ray Unable to be installed due to above error"
+					else
+						echo "[$CLUSTER_NODE_NAME] - Rex-Ray Installed"
+					fi
+					
+				else
+					echo "[$CLUSTER_NODE_NAME] - Rex-Ray Already INstalled, moving forward"
+				fi
+			EOSSH
+
+			echo "[$CLUSTER_NODE_NAME] - Configuring Rex-Ray"
+			docker-machine ssh "$CLUSTER_NODE_NAME"  sudo tee /etc/rexray/config.yml < "$STORAGE_PROVISION_CONFIG_FILE" > /dev/null
+			if [ $? -ne 0 ]; then
+				echo "[$CLUSTER_NODE_NAME] - Rex-Ray Unable to get configured due to above error"
 			else
-				echo "[$CLUSTER_NODE_NAME] - docker-compose already installed, moving forward"
+				echo "[$CLUSTER_NODE_NAME] - Rex-Ray configured"
+			fi
+			echo "[$CLUSTER_NODE_NAME] - Rex-Ray Restarting"
+			docker-machine ssh "$CLUSTER_NODE_NAME" sudo rexray start > /dev/null
+			if [ $? -ne 0 ]; then
+				echo "[$CLUSTER_NODE_NAME] - Rex-Ray Unable to Restart due to above error"
+			else
+				echo "[$CLUSTER_NODE_NAME] - Rex-Ray Restarted Successfully"
 			fi
 
-			echo "[$CLUSTER_NODE_NAME] - adding user ubuntu to group docker"
-			sudo usermod -aG docker ubuntu > /dev/null 2>&1
-			if [ $? -ne 0 ]; then 
-				echo "[$CLUSTER_NODE_NAME] - user ubuntu unable to be added to group docker"
-			else
-				echo "[$CLUSTER_NODE_NAME] - user ubuntu added to group docker successfully"
-			fi
-
-			if ! which rexray > /dev/null 2>&1; then 
-				echo "[$CLUSTER_NODE_NAME] - Installing Rex-Ray"
-				curl -sSL https://dl.bintray.com/emccode/rexray/install | sh > /dev/null 2>&1
-				echo "[$CLUSTER_NODE_NAME] - Rex-Ray Installed"
-				
-			else
-				echo "[$CLUSTER_NODE_NAME] - Rex-Ray Already INstalled, moving forward"
-			fi
+			echo "[$CLUSTER_NODE_NAME] - processing done"
+		) &
 		
-		EOSSH
+	done
 
-		echo "[$CLUSTER_NODE_NAME] - Configuring Rex-Ray"
-		docker-machine ssh "$CLUSTER_NODE_NAME"  sudo tee /etc/rexray/config.yml < "$STORAGE_PROVISION_CONFIG_FILE" > /dev/null 2>&1
-		echo "[$CLUSTER_NODE_NAME] - Rex-Ray configured"
-		echo "[$CLUSTER_NODE_NAME] - Rex-Ray Restarting"
-		docker-machine ssh "$CLUSTER_NODE_NAME" sudo rexray start > /dev/null 2>&1
-		echo "[$CLUSTER_NODE_NAME] - Rex-Ray Restarted Successfully"
+	echo "Wating for configuration of nodes..."
+	wait
+	echo "configuration of all nodes completed"
 
-		echo "[$CLUSTER_NODE_NAME] - processing done"
-	
-	) &
-	
-done
-
-echo "Wating for configuration of nodes..."
-wait
-echo "configuration of all nodes completed"
+else
+	echo "Configuration not required, moving forward"
+fi
 
 
 echo "Provisioning Successful"
